29d36752da388cdb837176009afc0e190fdd1d0d
T-Oda-BTO <85532743+T-Oda-BTO@users.noreply.github.com>
2021-06-11 16:49:00 +0900

    Add files via upload

--- 06Collar sensor/color.py
+++ 06Collar sensor/color.py
@@ -3,6 +3,7 @@ import time
 from cls381 import cls381
 
 CLS381_ADDR = 0x53
+LED_PIN = 19
 
 pi = pigpio.pi()
 I2C_CH = 1
@@ -11,10 +12,14 @@ color_sensor = cls381( pi, I2C_CH, CLS381_ADDR )
 
 color_sensor.set_mode( color_sensor.COLOR_MODE )
 
+pi.set_mode( LED_PIN, pigpio.OUTPUT )
+pi.write( LED_PIN, pigpio.LOW )
+
 while True:
     ( red, green, blue, ir ) = color_sensor.get_color()
     print( "Red :", red, "  Green :", green, "  Blue :", blue, "  IR :", ir )
 
     time.sleep( 0.5 )
+
 
 
--- 06Collar sensor/color_paper.py
+++ 06Collar sensor/color_paper.py
@@ -3,6 +3,7 @@ import time
 from cls381 import cls381
 
 CLS381_ADDR = 0x53
+LED_PIN = 19
 
 pi = pigpio.pi()
 I2C_CH = 1
@@ -10,6 +11,9 @@ I2C_CH = 1
 color_sensor = cls381( pi, I2C_CH, CLS381_ADDR )
 
 color_sensor.set_mode( color_sensor.COLOR_MODE )
+
+pi.set_mode( LED_PIN, pigpio.OUTPUT )
+pi.write( LED_PIN, pigpio.HIGH )
 
 while True:
     ( red, green, blue, ir ) = color_sensor.get_color()
